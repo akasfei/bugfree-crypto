a2613c01990f2ec8d5664017440c07578227c8b1
akasfei <akasfei@gmail.com>
2014-02-26 13:28:53 +0800

    RSA complete

--- ui/assets/js/application.js
+++ ui/assets/js/application.js
@@ -2,7 +2,7 @@ var fs = require('fs');
 var util = require('util');
 var exec = require('child_process').exec;
 
-var __dirname = process.execPath.replace(/nw\.exe/g, '');
+var __dirname = process.cwd();
 
 !function ($) {
 
@@ -11,11 +11,12 @@ var __dirname = process.execPath.replace(/nw\.exe/g, '');
 
     $('.rsa-gen-key').on('click', function (e) {
       var childProc;
-      childProc = exec('python ' + __dirname + 'rsa.py init', function (err) {
+      childProc = exec('python ' + __dirname + '/../rsa.py init', function (err, stdout) {
         if (err) {
           console.log(util.inspect(err));
           return;
         }
+        console.log(stdout);
         checkKey();
       });
     });
@@ -27,23 +28,54 @@ var __dirname = process.execPath.replace(/nw\.exe/g, '');
 
     $('#rsa-plain-path').on('change', function (e) {
       $('#rsa-plain-disp').val($(this).val());
+      $('#rsa-cipher-disp').val($(this).val() + '.rsa');
     });
 
     $('#rsa-cipher-path').on('change', function (e) {
       $('#rsa-cipher-disp').val($(this).val());
+      $('#rsa-plain-disp').val($(this).val() + '.plain');
+    });
+
+    $('#rsa-encrypt').on('click', function (e) {
+      var plainPath = $('#rsa-plain-disp').val();
+      var cipherPath = $('#rsa-cipher-disp').val();
+      var childProc;
+      childProc = exec('python ' + __dirname + '/../rsa.py encrypt ' + plainPath + ' ' + cipherPath, function (err, stdout) {
+        if (err) {
+          console.log(util.inspect(err));
+          return;
+        }
+        console.log(stdout);
+        window.alert('Encryption complete.');
+      });
+    });
+
+    $('#rsa-decrypt').on('click', function (e) {
+      var plainPath = $('#rsa-plain-disp').val();
+      var cipherPath = $('#rsa-cipher-disp').val();
+      var childProc;
+      childProc = exec('python ' + __dirname + '/../rsa.py decrypt ' + cipherPath + ' ' + plainPath, function (err, stdout) {
+        if (err) {
+          console.log(util.inspect(err));
+          return;
+        }
+        console.log(stdout);
+        window.alert('Decryption complete.');
+      });
     });
 })
 
 }(window.jQuery)
 
 var checkKey = function () {
-  if (fs.existsSync(__dirname + 'rsa.key')) {
-    var publicKey = fs.readFileSync(__dirname + 'rsa.pub', {encoding: 'utf8'});
-    var privateKey = fs.readFileSync(__dirname + 'rsa.key', {encoding: 'utf8'});
+  if (fs.existsSync(__dirname + '/rsa.key')) {
+    var publicKey = fs.readFileSync(__dirname + '/rsa.pub', {encoding: 'utf8'});
+    var privateKey = fs.readFileSync(__dirname + '/rsa.key', {encoding: 'utf8'});
     publicKey = publicKey.match(/(L[0-9]+L)/g);
     privateKey = privateKey.match(/(L[0-9]+L)/g);
-    $('#rsa-pub').val(publicKey.join(', ').replace(/L/g, ''));
-    $('#rsa-priv').val(privateKey.join(', ').replace(/L/g, ''));
+    $('#rsa-key-n').val(publicKey[0].replace(/L/g, ''));
+    $('#rsa-key-e').val(publicKey[1].replace(/L/g, ''));
+    $('#rsa-key-d').val(privateKey[1].replace(/L/g, ''));
   } else
     return;
 }
